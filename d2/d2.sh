3ebd0be8b86507d535a8cc9d4b9a3057dd140a71
Bastiaan Bakker <bbakker@xebia.com>
2017-12-03 00:23:06 +0100

    cleanup

--- d2/d2.sh
+++ d2/d2.sh
@@ -3,8 +3,7 @@
 checksum=0
 while read line ; do
   max=$(tr ' ' '\n' <<< $line | sort -n | tail -1)
-  min=$(tr ' ' '\n' <<< $line | sort -n -r | tail -1)
-  checksum=$((checksum + max - min))
-  echo $max $min $checksum
+  min=$(tr ' ' '\n' <<< $line | sort -n | head -1)
+  ((checksum += max - min))
 done
 echo $checksum
