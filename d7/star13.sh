a4f7847d04b3c495f67066d1f41d72cfddcdbbfe
Bastiaan Bakker <bbakker@xebia.com>
2017-12-07 11:22:38 +0100

    day 7 part 1 shortened

--- d7/star13.sh
+++ d7/star13.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cut -d\  -f1 < $1 | sort > nodes.txt
-egrep -- '->' $1 | cut -d\  -f4- | sed -e 's/, /\n/g' | sort > children.txt
-comm -3 nodes.txt children.txt
+egrep -o '[a-z]+' | sort | uniq -u
