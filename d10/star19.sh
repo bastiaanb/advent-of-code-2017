f16f5c8a8cfdcd8f3dcf699b6e4bf67efc9d5359
Bastiaan Bakker <bbakker@xebia.com>
2017-12-10 22:00:40 +0100

    remove echo's

--- d10/star19.sh
+++ d10/star19.sh
@@ -11,9 +11,6 @@ pos=0
 skipsize=0
 
 for l in $(tr ',' ' '); do
-  echo "$skipsize, $pos, $l: ${ring[*]}"
-
-  # rev pos l
   for((f=pos,b=pos+l-1; f < b; f++, b--)); do
     t=${ring[$((b % size))]}
     ring[$((b % size))]=${ring[$((f % size))]}
@@ -23,5 +20,4 @@ for l in $(tr ',' ' '); do
   ((skipsize++))
 done
 
-echo "$skipsize, $pos, $l: ${ring[*]}"
 echo $((${ring[0]} * ${ring[1]}))
