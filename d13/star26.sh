83f54802cf6188321c28eaafb0d8acafee7c2693
Bastiaan Bakker <bbakker@xebia.com>
2017-12-27 18:14:22 +0100

    use start26-b.sh instead of star26.sh

--- d13/star26.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sed 's/[:]//g' | {
-  while read depth range; do
-#    [[ $((depth % ((range - 1)*2) )) -eq 0 ]] && ((s+=depth*range))
-    m=$(((range - 1)*2))
-    echo -e "$((m/2))\t$(((depth % m)/2))"
-  done
-}
