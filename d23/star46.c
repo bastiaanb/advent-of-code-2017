a0c32d80cea1237876e48bfeab58872e48877d42
Bastiaan Bakker <bbakker@xebia.com>
2017-12-27 11:06:59 +0100

    day 23: fixed off by one, again

--- d23/star46.c
+++ d23/star46.c
@@ -3,19 +3,22 @@
 
 int main(int argc, char **argv) {
   long a=1, b=0, c=0, d=0, e=0, f=0, g=0, h=0;
-  for (b = 93*100 + 100000; b < 93*100 + 100000 + 17000; b += 17) {
+  if (a == 0) {
+    b = 93; c = 93;
+  } else {
+    b = 93*100 + 100000;
+    c = 93*100 + 100000 + 17000;
+  }
+  for (/**/; b <= c; b += 17) {
     f=1;
-    for (d=2; d != b; d++) {
-      for (e=2; e != b; e++) {
-        if (d * e - b == 0) {
-          f=0;
-        }
+    for (d=2; d < b; d++) {
+      if ((b % d) == 0) {
+        h++;
+        f=0;
+        break;
       }
-      d++;
     }
-    if (f == 0) {
-      h++;
-    }
+    printf("%ld: %ld %ld\n", b, h, f);
   }
   L33: printf("%ld\n", h);
 }
