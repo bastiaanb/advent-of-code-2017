d55cef612d886fb70fe1b39c0ab7c0eb03a75ccc
Bastiaan Bakker <bbakker@xebia.com>
2017-12-27 16:58:42 +0100

    day 14 part 2 cleanup

--- d14/star28.sh
+++ d14/star28.sh
@@ -1,55 +1,39 @@
 #!/bin/bash
 
-maze=()
-i=0
-size=128
-ls=$((size+2))
+cat <(printf '%.0s-' {1..128}; echo) - <(printf '%.0s-' {1..128}; echo) | {
+  maze=()
+  i=0
+  ls=130
 
-maze[$((i++))]='|'
-for((j=0; j<size; j++)) do
-  maze[$((i++))]='-'
-done
-maze[$((i++))]='|'
-
-while read line; do
-  maze[$((i++))]='|'
-  for((j=0; j < ${#line}; j++)) do
-    maze[$((i++))]=${line:$j:1}
-  done
-  maze[$((i++))]='|'
-done
-
-maze[$((i++))]='|'
-for((j=0; j < size; j++)) do
-  maze[$((i++))]='-'
-done
-maze[$((i++))]='|'
-
-clear
-echo "${maze[@]}" | sed -e 's/ //g' -e 's/||/|\n|/g'
-
-area=0
-
-stack=()
-sp=0
-
-pos=$((ls+1))
-while [[ ${maze[$pos]} != '-' ]]; do
-  if [[ ${maze[$pos]} == '#' ]]; then
-    ((area++))
-    echo "$(tput cup 0 $((ls+2)))$(tput setab 0)areas: $area$(tput setab $((1+area%7)))"
-    stack[$((sp++))]=$pos
-    while [[ $sp -gt 0 ]]; do
-      p=${stack[$((--sp))]}
-      if [[ ${maze[$p]} == '#' ]]; then
-        maze[$p]='X'
-        echo -n "$(tput cup $((p/ls)) $((p%ls))) "
-        stack[$((sp++))]=$((p-ls))
-        stack[$((sp++))]=$((p+ls))
-        stack[$((sp++))]=$((p-1))
-        stack[$((sp++))]=$((p+1))
-      fi
+  while read line; do
+    maze[$((i++))]='|'
+    for((j=0; j < ${#line}; j++)) do
+      maze[$((i++))]=${line:$j:1}
     done
-  fi
-  ((pos++))
-done
+    maze[$((i++))]='|'
+  done
+
+  clear
+  echo "${maze[@]}" | sed -e 's/ //g' -e 's/||/|\n|/g'
+
+  area=0
+  for pos in "${!maze[@]}"; do
+    if [[ ${maze[$pos]} == '#' ]]; then
+      ((area++))
+      echo "$(tput cup 0 $((ls+2)))$(tput setab 0)areas: $area$(tput setab $((1+area%7)))"
+      stack=($pos)
+      sp=1
+      while [[ $sp -gt 0 ]]; do
+        p=${stack[$((--sp))]}
+        if [[ ${maze[$p]} == '#' ]]; then
+          maze[$p]='X'
+          echo -n "$(tput cup $((p/ls)) $((p%ls))) "
+          stack[$((sp++))]=$((p-ls))
+          stack[$((sp++))]=$((p+ls))
+          stack[$((sp++))]=$((p-1))
+          stack[$((sp++))]=$((p+1))
+        fi
+      done
+    fi
+  done
+}
