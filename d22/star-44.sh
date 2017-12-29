d3038bdcce6950a6336df276f72f7c142cf95b53
Bastiaan Bakker <bbakker@xebia.com>
2017-12-29 16:37:59 +0100

    day 22 part 2 compacter version

--- d22/star-44.sh
+++ d22/star-44.sh
@@ -2,87 +2,25 @@
 
 declare -A grid
 
-plot() {
-  echo -n "$(tput cup $2 $1)$3"
-}
-
-turnright() {
-  case "$dir" in
-       up) dir=right ;;
-    right) dir=down  ;;
-     down) dir=left  ;;
-     left) dir=up    ;;
-  esac
-}
-
-turnleft() {
-  case "$dir" in
-       up) dir=left  ;;
-    right) dir=up    ;;
-     down) dir=right ;;
-     left) dir=down  ;;
-  esac
-}
-
-reverse() {
-  case "$dir" in
-       up) dir=down  ;;
-    right) dir=left  ;;
-     down) dir=up    ;;
-     left) dir=right ;;
-  esac
-}
-
+# states: 0=infected 1=flagged 2=empty 3=weakened
 y=0
-clear
 while read line; do
-  echo $line
   for((x=0;x<${#line};x++)); do
-    if [[ "${line:x:1}" == "#" ]]; then
-      grid["$x:$y"]="#"
-    fi
-    done
+    [[ "${line:x:1}" == "#" ]] && grid["$x:$y"]=0
+  done
   ((y++))
 done
 
-bottom="$(tput cup $y 0)"
-
-dir=up
-x=$((x/2))
-y=$((y/2))
-
+((x/=2, y/=2))
+dir=0
 j=0
 for((i=0; i<10000000; i++)); do
-  [[ $((i % 10000)) -eq 0 ]] && echo -e "i: $i\tj: $j\tx: $x\ty: $y"
-#  sleep 1
-  case "${grid[$x:$y]}" in
-    '#')
-      turnright
-      grid[$x:$y]="F"
-#      plot $x $y 'F'
-      ;;
-    'W')
-      ((j++))
-      grid[$x:$y]="#"
-#      plot $x $y '#'
-      ;;
-    'F')
-      reverse
-      unset "grid[$x:$y]"
-#      plot $x $y ' '
-      ;;
-    '')
-      turnleft
-      grid[$x:$y]="W"
-#      plot $x $y 'W'
-      ;;
-  esac
+  [[ $((i % 10000)) -eq 0 ]] && echo -e "i: $i\tj: d: $dir\t$j\tx: $x\ty: $y\tg: ${grid[$x:$y]}\ts: ${#grid[@]}\t\t"
 
-  case "$dir" in
-       up) ((y--)) ;;
-    right) ((x++)) ;;
-     down) ((y++)) ;;
-     left) ((x--)) ;;
-  esac
+  s=$(((${grid[$x:$y]-2}+1)%4))
+  grid[$x:$y]=$s
+  [[ s -eq 0 ]] && ((j++))
+  ((dir=(dir+s)%4))
+  case "$dir" in 0) ((y--)) ;; 1) ((x++)) ;; 2) ((y++)) ;; 3) ((x--)) ;; esac
 done
-echo -e "i: $i\tj: $j\tx: $x\ty: $y"
+echo -e "j: $j\tx: $x\ty: $y\ts: ${#grid[@]}\t"
